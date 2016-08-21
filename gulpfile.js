a0a6a733b022ee6556d6173f5dd1f9e6bf578e2f
root <weiding8911@gmail.com>
2016-08-21 17:44:24 +0000

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ gulp.task('miniJS', function() {
            './public/javascripts/third_party/lodash.min.js',
            './public/javascripts/third_party/bootstrap.min.js',
            './public/javascripts/third_party/bootstrap-toggle.min.js',
+           './public/javascripts/third_party/bootstrap-multiselect.js',
            './public/javascripts/third_party/jquery.bootstrap-autohidingnavbar.min.js',
            './public/javascripts/third_party/call-autohidingnavbar.js',
            './public/javascripts/third_party/d3.min.js',
@@ -27,7 +28,7 @@ gulp.task('miniJS', function() {
            './public/javascripts/third_party/dataTables.scrollingPagination.js',
            './public/javascripts/third_party/dataTables.bootstrap.min.js',
            './public/javascripts/third_party/dataTables.autoFill.min.js',
-           './public/javascripts/third_party/msa-new.min.js',
+           './public/javascripts/third_party/msa-new.js',
            './public/javascripts/third_party/tnt.tree.min.js',
            './public/javascripts/third_party/jquery.panzoom.min.js',
            './public/javascripts/reload.test2.min.js',
@@ -44,13 +45,17 @@ gulp.task('miniCSS', function() {
 	gulp.src(['./public/stylesheets/pace-theme-center-atom.css',
              './public/stylesheets/nouislider.min.css',
              './public/stylesheets/bootstrap.min.css',
+             './public/stylesheets/bootstrap-multiselect.css',
              './public/stylesheets/bootstrap-toggle.min.css',
              './public/stylesheets/msa-new.css',
              './public/stylesheets/footer.css',
              './public/stylesheets/page-appearance.css',
              './public/stylesheets/autohiding-navbar2.css',
-             './public/stylesheets/d3-tip.css','./public/stylesheets/dc.css',
-             './public/stylesheets/dataTables.bootstrap.css'])
+             './public/stylesheets/d3-tip.css',
+             './public/stylesheets/dc.css',
+             './public/stylesheets/dataTables.bootstrap.css',
+             './public/stylesheets/dataTables-customized.css'
+             ])
 	    .pipe(minifyCSS({processImport: false}))
 	    //.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
 	    .pipe(concat('all.css'))
