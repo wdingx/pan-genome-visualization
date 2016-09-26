c53b9b31dec111c0d14f4416c138c3a620fb6960
root <weiding8911@gmail.com>
2016-09-26 07:50:02 +0000

    add new pages in views

---
+++ add-new-pages-repo.sh
@@ -0,0 +1,13 @@
+## Usage: bash add-new-pages-repo.sh your_species_prefix
+#species_prefix
+
+species=$1
+
+# process jade (in folder ./views/)
+mkdir ./public/dataset/$species
+mkdir ./public/dataset/$species/geneCluster
+cp ./views/instance.jade ./views/$species.jade
+sed -i -- "s/TestSet/$species/g" ./views/$species.jade
+# process js (in folder ./routes/)
+cp ./routes/TestSet.js ./routes/$species.js
+sed -i -- "s/TestSet/$species/g" ./routes/$species.js
