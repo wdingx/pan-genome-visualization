68a6788aaa8780beec6933dce506b321788898c1
wding <csddsc10@126.com>
2017-01-18 16:19:26 +0100

    move datatable configuration in this script

--- public/javascripts/datatable-gc.js
+++ public/javascripts/datatable-gc.js
@@ -89,3 +89,76 @@ $.fn.dataTable.ext.errMode = 'none';
 if (1) {
     $('#dc-data-table').on('error.dt', function(e, settings, techNote, message) { console.log(message); });
 }
+
+
+
+function datatable_configuration(table_input) {
+    //## datatable configuration
+    datatable = $('#dc-data-table').DataTable({
+        responsive: true,
+        //dom: 'Bfrtip',
+        //buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
+        'paging': true,
+        //'pagingType': 'full_numbers',
+        'scrollX': true,
+        'scrollY': '200px',//'30vh',
+        //'scrollCollapse': true,
+        colReorder: true,
+        'bAutoWidth': true,
+        //'bDeferRender': true,
+        'deferRender':    true,
+        'aaData': table_input,
+        //'bDestroy': true,
+        /*"processing": true, "serverSide": true,*/
+        'columnDefs': dc_dataTable_columnDefs_config,
+        // order by count (desc) and geneId (asc)
+        "order": [[2, 'desc' ],[9, 'asc' ]]
+    });
+
+    $('<span style="display:inline-block; width: 10px;"></span>').appendTo('div#dc-data-table_length.dataTables_length');
+    $('<select id="GC_tablecol_select" multiple="multiple" ></select>').appendTo('div#dc-data-table_length.dataTables_length');
+
+    //## empty and non-empty indexes
+    function get_all_Indexes(array) {
+        var non_empty_indexes = []; var empty_indexes = []; 
+        var dropdown_table_col = []; var i;
+        for(i = 0; i < array.length; i++) {
+            if (array[i] === '') {empty_indexes.push(i);}
+            else { non_empty_indexes.push(i) }
+        }
+        return [non_empty_indexes, empty_indexes];
+    }
+
+    var indexes_list= get_all_Indexes(geneCluster_table_columns, '');
+    var non_empty_index_list= indexes_list[0]; 
+    var empty_inde_list = indexes_list[1]; 
+
+    creat_multiselect("#GC_tablecol_select",GC_table_dropdown_columns);
+    $('#GC_tablecol_select').multiselect({
+        //enableFiltering: true,
+        onChange: function(element, checked) {
+
+            function element_included (arr, number) {
+                return (arr.indexOf(number) != -1)
+            }
+            var col_index = GC_table_dropdown_columns.indexOf(element.val());
+            var original_col_index = non_empty_index_list[col_index];
+
+            if (checked === true) {
+                if ( element_included(empty_inde_list,original_col_index-1)==true ) {
+                    var column_expand = datatable.column( original_col_index-1 );
+                    column_expand.visible( ! column_expand.visible() );}
+                var column_normal = datatable.column( original_col_index );
+                column_normal.visible( ! column_normal.visible() );
+            }
+            else if (checked === false) {
+                if ( element_included(empty_inde_list,original_col_index-1)==true ) {
+                    var column_expand = datatable.column( original_col_index-1 );
+                    column_expand.visible( ! column_expand.visible() );}
+                var column_normal = datatable.column( original_col_index );
+                column_normal.visible( ! column_normal.visible() ); 
+
+            }
+        }  
+    });
+}
