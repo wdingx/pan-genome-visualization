e62c3b7b7367702c21f3adb7e49c95d366b8a97a
wding <csddsc10@126.com>
2017-02-19 23:20:06 +0100

    datapaths added

---
+++ public/javascripts/data_path-compare.js
@@ -0,0 +1,9 @@
+//var speciesAbbr='Sa'
+var path_datatable1="./dataset/"+speciesAbbr+"/geneCluster.json",
+path_datatable11="./dataset/"+speciesAbbr+"/geneCluster2.json",
+path_datatable2="./dataset/"+speciesAbbr+"/strainMetainfo.json",
+aln_file_path="./dataset/"+speciesAbbr+"/geneCluster/",
+coreTree_path="./dataset/"+speciesAbbr+"/coreGenomeTree.json",
+coreTree_path_B="./dataset/"+speciesAbbr+"/coreGenomeTree2.json",
+sequenceJsonPath="./dataset/"+speciesAbbr+"/_seq.json";
+//nodeAttriPath="./dataset/"+speciesAbbr+"/-tnt-nodeAttri.json"
