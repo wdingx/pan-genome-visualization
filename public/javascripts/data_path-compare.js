a7165d592261166ed3fc4716cb71c9923cb1aeb3
wding <csddsc10@126.com>
2017-02-27 09:14:21 +0100

    update data path

--- public/javascripts/data_path-compare.js
+++ public/javascripts/data_path-compare.js
@@ -1,13 +1,17 @@
-//var speciesAbbr='Sa'
-var speciesAbbr2=speciesAbbr+'2';
+/** data paths */
+if (typeof speciesAbbr2=='undefined') {
+	var speciesAbbr2=speciesAbbr+'2';
+};
 var path_datatable1="./dataset/"+speciesAbbr+"/geneCluster.json",
 coreTree_path="./dataset/"+speciesAbbr+"/coreGenomeTree.json",
 aln_file_path="./dataset/"+speciesAbbr+"/geneCluster/",
 path_datatable_meta="./dataset/"+speciesAbbr+"/strainMetainfo.json",
+geneEvent_path_A='./dataset/'+speciesAbbr+'/geneGainLossEvent.json'
 
 path_datatable11="./dataset/"+speciesAbbr2+"/geneCluster.json",
 coreTree_path_B="./dataset/"+speciesAbbr2+"/coreGenomeTree.json",
 aln_file_path_B="./dataset/"+speciesAbbr2+"/geneCluster/",
+geneEvent_path_B='./dataset/'+speciesAbbr2+'/geneGainLossEvent.json'
 
 sequenceJsonPath="./dataset/"+speciesAbbr+"/_seq.json";
 //nodeAttriPath="./dataset/"+speciesAbbr+"/-tnt-nodeAttri.json"
