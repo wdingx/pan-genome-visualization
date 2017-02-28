4d3269051dfb5a829f6939191e6906a168f7344b
wding <csddsc10@126.com>
2017-02-28 14:19:08 +0100

    move dropdown(meta) in render_viewer.js

--- public/javascripts/render_viewer.js
+++ public/javascripts/render_viewer.js
@@ -6,6 +6,10 @@ render_tree(0, "compare_tree1", coreTree_path_B, clusterID=null, 1);
 rotate_monitor('tree_rotate_01','mytree2');
 rotate_monitor('tree_rotate_02','compare_tree2');
 
+/** create metadata dropdown list */
+creat_dropdown("#dropdown_list_01",'mytree1','mytree2',0);
+creat_dropdown("#dropdown_list_02",'compare_tree1','compare_tree2',1);
+
 /** render interactive charts and datatables */
 render_chart_table.initData(path_datatable1,'dc_data_table1', 'GC_tablecol_select',
     'dc_data_count','dc_straincount_chart','dc_geneLength_chart','dc_coreAcc_piechart',
