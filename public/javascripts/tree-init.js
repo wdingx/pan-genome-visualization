ef2ae913316986fefdb8dbde65f9d84f585d6103
wding <csddsc10@126.com>
2017-02-28 12:28:42 +0100

    add store_tree_style/store_genePattern_style (also restore...) to deal with color recalling with 2 dashboards(tools)

--- public/javascripts/tree-init.js
+++ public/javascripts/tree-init.js
@@ -1,27 +1,51 @@
 var pgModule = function(){
-    var current = null;
-    var labels = {'home':'home'};
-    var init = function(){
-    };
-
-    function hasOwnProperty(obj, prop){
+    var hasOwnProperty= function(obj, prop){
         return (obj[prop] !== undefined);
     }
-    
-   /*function hasOwnProperty(obj, prop) {
+    /*function hasOwnProperty(obj, prop) {
         var proto = obj.__proto__ || obj.constructor.prototype;
         return (prop in obj) &&
             (!(prop in proto) || proto[prop] !== obj[prop]);
     }*/
 
-    function isEmptyObj(obj) {
-        for (var key in obj) { 
+    var isEmptyObj= function(obj) {
+        for (var key in obj) {
             if (obj.hasOwnProperty(key)) { return false; }
         }
         return true;
+    };
+
+    var store_tree_style= function(tool_side, style_type, target_type, current_style) {
+        if (tool_side==1) {//pxTree.genePattern_tool2['node_color_mem'][d.name]=node_color
+            pxTree.treeStyle_tool2[style_type][target_type]=current_style
+        } else {
+            pxTree.treeStyle_tool1[style_type][target_type]=current_style
+        }
     }
 
-   return{ hasOwnProperty:hasOwnProperty, isEmptyObj:isEmptyObj }
+    var restore_tree_style= function(tool_side, style_type, target_type) {
+        return (tool_side==1) ? pxTree.treeStyle_tool2[style_type][target_type] : pxTree.treeStyle_tool1[style_type][target_type];
+    }
+
+    var store_genePattern_style= function(tool_side, style_type, target_type, current_style) {
+        if (tool_side==1) {
+            pxTree.genePattern_tool2[style_type][target_type]=current_style
+        } else {
+            pxTree.genePattern_tool1[style_type][target_type]=current_style
+        }
+    }
+
+    var restore_genePattern_style= function(tool_side, style_type, target_type) {
+        return (tool_side==1) ? pxTree.genePattern_tool2[style_type][target_type] : pxTree.genePattern_tool1[style_type][target_type];
+    }
+
+   return{  hasOwnProperty:hasOwnProperty,
+            isEmptyObj:isEmptyObj,
+            store_tree_style:store_tree_style,
+            restore_tree_style:restore_tree_style,
+            store_genePattern_style:store_genePattern_style,
+            restore_genePattern_style:restore_genePattern_style,
+        }
 }();
 
 var pgDashbord = {
@@ -29,26 +53,39 @@ var pgDashbord = {
 };
 
 var pxTree = {
-    large_output: true, //if true, use separated pattern instead of entire pattern
+    /**if true, use separated pattern instead of entire pattern */
+    large_output: false,
     gain_loss_enabled: true,
     id: 5,
     collapsed_node_size: 4.5,
     collapsed_node_fill: '#26B629', //'steelblue',
     collapsed_node_stroke:'steelblue',
-    branch_col: '#4A4A4A', 
+    branch_col: '#4A4A4A',
     branch_col_highlight: '#2D59B1',
     branch_wid_highlight: '3px',
     link_width: '1px',
     link_dasharray: '1px, 0px',
     col_pres: '#005BCC',//'#3A89EA' '#1F69C4' gene presence
     col_abse: '#E01F1F', // '#D82400' '#EA5833'; gene absence
-    col_node_stroke:'#fff',
-    node_color_mem: {},
-    link_color_mem: {}, 
-    link_width_mem: {}, 
-    link_dash_mem: {},
+    node_metaunknown_stroke:'#FFFFFF',
     wid_link: '1.2px',
     wid_gloss: '3px',//gain loss highlight
+    genePattern_tool1: {
+        node_color_mem: {}, link_color_mem: {},
+        link_width_mem: {}, link_dash_mem:  {}
+    },
+    genePattern_tool2: {
+        node_color_mem: {}, link_color_mem: {},
+        link_width_mem: {}, link_dash_mem:  {}
+    },
+    treeStyle_tool1: {
+        node_color_mem: {}, link_color_mem: {},
+        link_width_mem: {}, link_dash_mem:  {}
+    },
+    treeStyle_tool2: {
+        node_color_mem: {}, link_color_mem: {},
+        link_width_mem: {}, link_dash_mem:  {}
+    }
 };
 var treeSwitch= {
     layout_vertical: 'false',
