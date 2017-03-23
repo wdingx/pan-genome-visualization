693d8e6d6e705bbe377c1e9d848f1ed1fffac215
wding <weiding8911@gmail.com>
2017-03-23 14:21:16 +0100

    meta-datatable is back

--- public/javascripts/datatable-meta.js
+++ public/javascripts/datatable-meta.js
@@ -1,22 +1,24 @@
-//# create meta table
-var meta_table_id='dc_data_table_meta'
-var meta_table_columns= Object.keys(meta_display_set);
-meta_table_columns.unshift('accession');
-creat_dataTable('#'+meta_table_id,meta_table_columns);
-
+import {create_dataTable} from "./datatable-gc";
+import {path_datatable_meta} from './data_path';
 //#DataTable for meta-info
-var chartExample2 = {
+export const chartExample2 = {
     dataTable2Fun: function (meta_table_id) {
 
+        //# create meta table
+        var meta_table_columns= Object.keys(meta_display_set);
+        meta_table_columns.unshift('accession');
+        create_dataTable('#'+meta_table_id,meta_table_columns);
+
+        //# assign data to each column
         var columnDefs_list=[];
-        for (i = 0; i < meta_table_columns.length; i++) {
+        for (var i = 0; i < meta_table_columns.length; i++) {
             columnDefs_list.push({
                 'targets': i,
                 'data': meta_table_columns[i]
             });
         }
 
-        datatable2= $('#'+meta_table_id).DataTable({
+        var datatable2= $('#'+meta_table_id).DataTable({
                 'ajax': path_datatable_meta,
                 'responsive': true,
                 'search':true,
@@ -32,4 +34,4 @@ var chartExample2 = {
             });
     }
 };
-chartExample2.dataTable2Fun(meta_table_id);
+//chartExample2.dataTable2Fun(meta_table_id);
--- public/javascripts/render_viewer.js
+++ public/javascripts/render_viewer.js
@@ -2,7 +2,7 @@ import {render_chart_table} from "./interact";
 //import {create_dropdown} from "./meta-color-legend";
 import * as datapath from "./data_path";
 import speciesTree from "./speciesTree";
-
+import  {chartExample2} from "./datatable-meta";
 // /** strain_tree processing */
 //render_tree(0, "mytree1", coreTree_path, clusterID=null, null);
 
@@ -20,9 +20,13 @@ speciesTree("speciesTree", datapath.coreTree_path, handleSpeciesTree);
 /** create metadata dropdown list */
 //create_dropdown("#dropdown_list",'mytree1','mytree2','coreTree_legend',null);
 
-/** render interactive charts and datatables */
-console.log("render_viewer:",datapath);
+/** render interactive charts and gene-cluster datatable */
+//console.log("render_viewer:",datapath);
 render_chart_table.initData(datapath.path_datatable1,'dc_data_table', 'GC_tablecol_select',
     'dc_data_count','dc_straincount_chart','dc_geneLength_chart','dc_coreAcc_piechart',
     'changeCoreThreshold','coreThreshold',
     'speciesTreeDiv','geneTreeDiv', null);
+
+/** render meta-data datatable */
+var meta_table_id='dc_data_table_meta';
+chartExample2.dataTable2Fun(meta_table_id);
