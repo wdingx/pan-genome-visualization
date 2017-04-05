9679ef3911989f5a179fe9afc8766dcc32175e9a
wding <weiding8911@gmail.com>
2017-04-05 11:31:20 +0200

    update color sets

--- public/javascripts/colors.js
+++ public/javascripts/colors.js
@@ -1,14 +1,13 @@
-//## color-blind friendly color set http://colorbrewer2.org/
-export const safe_color_set=["#a50026","#d73027","#f46d43","#fdae61","#fee090","#ffffbf","#e0f3f8","#abd9e9","#74add1","#4575b4","#313695"];
-// d3.scale.category20();
-//export const more_color_set =  ["#3182bd", "#6baed6", "#9ecae1", "#c6dbef", "#e6550d", "#fd8d3c", "#fdae6b", "#fdd0a2", "#31a354", "#74c476", "#a1d99b", "#c7e9c0", "#756bb1", "#9e9ac8", "#bcbddc", "#dadaeb", "#636363", "#969696", "#bdbdbd", "#d9d9d9" ];
-// d3.range(20).map(d3.scale.category20())
-export const more_color_set = ["#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a", "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94", "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d", "#17becf", "#9edae5"]
+//** 12-color palette(colorblind safe)
+//** from http://mkweb.bcgsc.ca/colorblind/img/colorblindness.palettes.simple.png
+export const colorblind_safe_set = ['#006E82','#8214A0','#005AC8','#00A0FA','#FA78FA','#14D2DC','#AA0A3C','#FA7850','#0AB45A','#F0F032','#A0FA82','#FAE6BE']
+//** More:15-color palette for common color blindness types from http://mkweb.bcgsc.ca/biovis2012/
+// d3.scale.category20();// d3.range(20).map(d3.scale.category20())
 
+export const discrete_color_set = ['#F02132','#EA761F','#F5B223','#38A920','#2195C3','#1D52D3','#9E04A9']
 
-export const sequential_color_set= ['#313695','#4575b4','#74add1','#abd9e9','#e0f3f8','#ffffbf','#fee090','#fdae61','#f46d43','#d73027','#a50026']
-//# colorbind-safe http://colorbrewer2.org
-//# 11-class RdYlBu
-//# ['#a50026','#d73027','#f46d43','#fdae61','#fee090','#ffffbf','#e0f3f8','#abd9e9','#74add1','#4575b4','#313695']
-//# 9-class YlGnBu
-//# ['#ffffd9','#edf8b1','#c7e9b4','#7fcdbb','#41b6c4','#1d91c0','#225ea8','#253494','#081d58']
+//** 11-color palette for diverging scheme (colorbind safe, RdYlBu) from http://colorbrewer2.org
+export const continuous_color_set= ['#313695','#4575b4','#74add1','#abd9e9','#e0f3f8','#ffffbf','#fee090','#fdae61','#f46d43','#d73027','#a50026']
+//** other
+//** 9-class YlGnBu
+//** ['#ffffd9','#edf8b1','#c7e9b4','#7fcdbb','#41b6c4','#1d91c0','#225ea8','#253494','#081d58']
