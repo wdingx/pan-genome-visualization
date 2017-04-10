86e8372dde67ab7bd5878dffb7f31267e18d5742
wding <weiding8911@gmail.com>
2017-04-10 22:02:02 +0200

    distinguish onTipHover(blue highlight) and onTipsHover(colorful highlight) in geneTree

--- public/javascripts/geneTreeCallbacks.js
+++ public/javascripts/geneTreeCallbacks.js
@@ -1,12 +1,26 @@
 import {preOrderIteration} from "../phyloTree/src/treeHelpers";
+import {panXTree} from './global';
+
+const tipFillHover = panXTree.tipFillHover,
+      tipParalogFillHover = panXTree.tipParalogFillHover;
+const subtree_node_colors = d3.scale.category20c();
+
+const onTipsHover = function(d){
+  d.elem.attr("r",10)
+    .style("fill", function(x){return subtree_node_colors(x.n.accession);});
+  d.strainTip.attr("r",10)
+    .style("fill", function(x){return subtree_node_colors(x.n.name);});
+  for (var gi=0,len=d.paralogs.length; gi<len; gi++){
+    d.paralogs[gi].elem.attr("r",10)
+      .style("fill", function(x){return subtree_node_colors(x.n.accession);});
+    }
+};
 
 const onTipHover = function(d){
-  d.elem.attr("r",10).style("fill", "#5AE");
-  d.strainTip.attr("r",10).style("fill", function(x){return d3.rgb(x.tipAttributes.fill).brighter();});
-  for (var gi=0; gi<d.paralogs.length; gi++){
-    d.paralogs[gi].elem
-      .attr("r",10)
-      .style("fill","#81E");
+  d.elem.attr("r",function(x){return x.tipAttributes.r*2;}).style("fill", tipFillHover);
+  d.strainTip.attr("r",function(x){return x.tipAttributes.r*2;}).style("fill", tipFillHover);
+  for (var gi=0,len=d.paralogs.length; gi<len; gi++){
+    d.paralogs[gi].elem.attr("r",function(x){return x.tipAttributes.r*2;}).style("fill",tipParalogFillHover);//"#81E"
     }
 };
 
@@ -25,16 +39,17 @@ const onTipLeave = function(d){
 };
 
 
-
 const onBranchHover = function(d){
-  preOrderIteration(d, function(x){if (x.terminal){onTipHover(x);}});
+  if (d.n.children==undefined){
+    onTipHover(d);
+  }else{
+    preOrderIteration(d, function(x){if (x.terminal){onTipsHover(x);}});
+  }
 };
 
 const onBranchLeave = function(d){
   preOrderIteration(d, function(x){if (x.terminal){onTipLeave(x);}});
 };
-
-
 
 const geneTreeCallbacks = {
   onBranchClick:function(d){console.log(d.n.name);},
