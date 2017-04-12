1358d921956dd37bec005e1c0d159d914bf4d6f1
wding <weiding8911@gmail.com>
2017-04-12 14:14:01 +0200

    state.selected should have tristate(undefined,true,false)

--- public/javascripts/speciesTreeCallbacks.js
+++ public/javascripts/speciesTreeCallbacks.js
@@ -37,9 +37,21 @@ const onTipHover = function(d){
 
 const onTipLeave = function(d){
   d.elem
-    .attr("r",function(x){return x.state.selected ? x.tipAttributes.r*1.5 : x.tipAttributes.r*0.5;})
+    .attr("r",function(x){
+      if (x.state.selected==undefined){
+        return x.tipAttributes.r;
+      }else{
+        return x.state.selected ? x.tipAttributes.r*1.5 : x.tipAttributes.r*0.5;
+      }
+    })
     .style("stroke",function(x){return x.tipAttributes.fill;})
-    .style("fill",function(x){return x.state.selected ? x.tipAttributes.fill : tipUnselected;});
+    .style("fill",function(x){
+      if (x.state.selected==undefined){
+        return x.tipAttributes.fill;
+      }else{
+        return x.state.selected ? x.tipAttributes.fill : tipUnselected;
+      }
+    });
     //.style("fill",function(x){return x.tipAttributes.fill;});
   for (var gi=0; gi<d.genes.length; gi++){
     d.genes[gi].elem
