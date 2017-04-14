aab2173059690d3cdb5a03d157c8c189b5815807
wding <weiding8911@gmail.com>
2017-04-14 12:01:27 +0200

    if strain_id is not in attr, use d.n.name(accession) as tipText

--- public/javascripts/tree-init.js
+++ public/javascripts/tree-init.js
@@ -91,8 +91,8 @@ export const branchText = function(d){
 }
 export const branchFontSize = function(d){return d.stats.leafCount>2?3:0;}
 export const tipText = function(d){
-    if (d.n.attr.strain && d.terminal){
-        return d.n.attr.strain;
+    if (d.terminal){
+        return d.n.attr.strain ? d.n.attr.strain: d.n.name ;
     }else{
         return "";
     }
