d76eeb27f9cf51f4338709c99166f565ec5b5fc0
wding <weiding8911@gmail.com>
2017-05-04 19:20:54 +0200

    update PAO1 data

--- public/dataset/P_aeruginosa/newColumnConfig.js
+++ public/dataset/P_aeruginosa/newColumnConfig.js
@@ -2,7 +2,7 @@ var new_columns_config=[
 	{insertion_pos:'Name',
 	 col_header:'PAO1',
 	 new_col: {
-        'data':'Paref',
+        'data':'PAO1',
         'render':
             function(data, type, row, meta){
                 return ' <a href="http://pseudomonas.com/primarySequenceFeature/list?strain_ids=107&term=Pseudomonas+aeruginosa+PAO1+%28Reference%29&c1=name&v1='+data+'+&e1=1&assembly=complete" target="_blank">'+data+'</a>';
