9126ca3cb8f5a84be5013cf0264512e752efc1fd
wding <weiding8911@gmail.com>
2017-06-15 13:58:21 +0200

    update S_pneumoniae616 newColumnConfig.js

--- public/dataset/S_pneumoniae616/newColumnConfig.js
+++ public/dataset/S_pneumoniae616/newColumnConfig.js
@@ -1,17 +1,21 @@
 var apply_association_cols=true;
 var new_columns_config=[
     //branch association
-    {insertion_pos:'Length', new_col: {'data':'Ceftriaxone BA','visible': false, 'name':'Ceftriaxone BA','tooltip':'Ceftriaxone branch association'}},
-    {insertion_pos:'Length', new_col: {'data':'Tetracycline BA','visible': false, 'name':'Tetracycline BA','tooltip':'Tetracycline branch association'}},
-    {insertion_pos:'Length', new_col: {'data':'Erythromycin BA','visible': false, 'name':'Erythromycin BA','tooltip':'Erythromycin branch association'}},
-    {insertion_pos:'Length', new_col: {'data':'Trimethoprim BA','visible': false, 'name':'Trimethoprim BA','tooltip':'Trimethoprim branch association'}},
-    {insertion_pos:'Length', new_col: {'data':'Chloramphenicol BA','visible': false, 'name':'Chloramphenicol BA','tooltip':'Chloramphenicol branch association'}},
-    {insertion_pos:'Length', new_col: {'data':'Benzylpenicillin BA','visible': false, 'name':'Benzylpenicillin BA','tooltip':'Benzylpenicillin branch association'}},
+    //{insertion_pos:'Length', new_col: {'data':'', 'group':'association', 'visible': true, 'group name':'All branch- and presence- association','tooltip':'branch association'}},
+    {insertion_pos:'Length', new_col: {'data':'', 'group name':'Branch association', 'visible': true, 'tooltip':'branch association'}},
+    {insertion_pos:'Length', new_col: {'data':'Ceftriaxone BA', 'group':'Branch association', 'visible': true, 'name':'Ceftriaxone BA','tooltip':'Ceftriaxone branch association'}},
+    {insertion_pos:'Length', new_col: {'data':'Tetracycline BA', 'group':'Branch association', 'visible': true, 'name':'Tetracycline BA','tooltip':'Tetracycline branch association'}},
+    {insertion_pos:'Length', new_col: {'data':'Erythromycin BA', 'group':'Branch association', 'visible': true, 'name':'Erythromycin BA','tooltip':'Erythromycin branch association'}},
+    {insertion_pos:'Length', new_col: {'data':'Trimethoprim BA', 'group':'Branch association', 'visible': true, 'name':'Trimethoprim BA','tooltip':'Trimethoprim branch association'}},
+    {insertion_pos:'Length', new_col: {'data':'Chloramphenicol BA', 'group':'Branch association', 'visible': true, 'name':'Chloramphenicol BA','tooltip':'Chloramphenicol branch association'}},
+    {insertion_pos:'Length', new_col: {'data':'Benzylpenicillin BA', 'group':'Branch association', 'visible': true, 'name':'Benzylpenicillin BA','tooltip':'Benzylpenicillin branch association'}},
+
     //presence association
-    {insertion_pos:'Length', new_col: {'data':'Ceftriaxone PA','visible': false, 'name':'Ceftriaxone PA', 'tooltip':'Ceftriaxone presence/absence association'}},
-    {insertion_pos:'Length', new_col: {'data':'Tetracycline PA','visible': false, 'name':'Tetracycline PA','tooltip':'Tetracycline presence/absence association'}},
-    {insertion_pos:'Length', new_col: {'data':'Erythromycin PA','visible': false, 'name':'Erythromycin PA','tooltip':'Erythromycin presence/absence association'}},
-    {insertion_pos:'Length', new_col: {'data':'Trimethoprim PA','visible': false, 'name':'Trimethoprim PA','tooltip':'Trimethoprim presence/absence association'}},
-    {insertion_pos:'Length', new_col: {'data':'Chloramphenicol PA','visible': false, 'name':'Chloramphenicol PA','tooltip':'Chloramphenicol presence/absence association'}},
-    {insertion_pos:'Length', new_col: {'data':'Benzylpenicillin PA','visible': false, 'name':'Benzylpenicillin PA','tooltip':'Benzylpenicillin presence/absence association'}}
+    {insertion_pos:'Length', new_col: {'data':'', 'group name':'presence/absence association', 'visible': true, 'tooltip':'presence/absence association'}},
+    {insertion_pos:'Length', new_col: {'data':'Ceftriaxone PA', 'group':'presence/absence association', 'visible': true, 'name':'Ceftriaxone PA', 'tooltip':'Ceftriaxone presence/absence association'}},
+    {insertion_pos:'Length', new_col: {'data':'Tetracycline PA', 'group':'presence/absence association', 'visible': true, 'name':'Tetracycline PA','tooltip':'Tetracycline presence/absence association'}},
+    {insertion_pos:'Length', new_col: {'data':'Erythromycin PA', 'group':'presence/absence association', 'visible': true, 'name':'Erythromycin PA','tooltip':'Erythromycin presence/absence association'}},
+    {insertion_pos:'Length', new_col: {'data':'Trimethoprim PA', 'group':'presence/absence association', 'visible': true, 'name':'Trimethoprim PA','tooltip':'Trimethoprim presence/absence association'}},
+    {insertion_pos:'Length', new_col: {'data':'Chloramphenicol PA', 'group':'presence/absence association', 'visible': true, 'name':'Chloramphenicol PA','tooltip':'Chloramphenicol presence/absence association'}},
+    {insertion_pos:'Length', new_col: {'data':'Benzylpenicillin PA', 'group':'presence/absence association', 'visible': true, 'name':'Benzylpenicillin PA','tooltip':'Benzylpenicillin presence/absence association'}}
 ];
