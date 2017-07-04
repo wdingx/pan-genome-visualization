b32ee9df8f2758c8d96ea1dc14587b15aff54056
wding <weiding8911@gmail.com>
2017-07-04 20:37:47 +0200

    update index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'panX' });
+  res.render('home', { title: 'panX' });
   //res.sendfile(__dirname + '/public/clienthtml/index.html');
 });
 
