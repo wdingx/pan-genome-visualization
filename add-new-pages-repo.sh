b34c074601e68fc0f4c4a26bc3f1078b416b0b83
wding <weiding8911@gmail.com>
2017-10-02 09:46:04 +0200

    update layout options

--- add-new-pages-repo.sh
+++ add-new-pages-repo.sh
@@ -1,11 +1,23 @@
-## Usage: bash add-new-pages-repo.sh your_species_prefix
+## Usage:
+## standard layout (gene cluster table and alignment on the same row):
+##	bash add-new-pages-repo.sh your_species
+## wide layout (gene cluster table in one row and alignment at the bottom):
+##	bash add-new-pages-repo.sh your_species wide
 
-species=$1
-# process jade (in folder ./views/)
+species=$1;
+layout=$2;
+# process jade file (in folder ./views/)
 mkdir -p ./public/dataset/$species
-#mkdir -p ./public/dataset/$species/geneCluster
-cp ./views/instance.jade ./views/$species.jade
+if [ "$layout" == "wide" ]; then
+	echo 'using wide layout';
+	cp ./views/instance-wide.jade ./views/$species.jade
+else
+	echo 'using standard layout';
+	cp ./views/instance.jade ./views/$species.jade
+fi
+
 sed -i -- "s/TestSet/$species/g" ./views/$species.jade
-# process js (in folder ./routes/)
+
+# process js file (in folder ./routes/)
 cp ./routes/TestSet.js ./routes/$species.js
 sed -i -- "s/TestSet/$species/g" ./routes/$species.js
