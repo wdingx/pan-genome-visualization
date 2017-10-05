6ee85b6551e153e581eac660551aee8e5f70740e
wding <weiding8911@gmail.com>
2017-10-05 13:58:01 +0200

    add case for dataset that does not have proper accession info

--- public/javascripts/datatable-meta.js
+++ public/javascripts/datatable-meta.js
@@ -10,7 +10,12 @@ export const metaDataTable = {
         var metaDatatable, metaConfiguration;
         //# create meta table
         var meta_display_order = meta_display['meta_display_order'].slice();
-        meta_display_order.unshift('accession','strain');
+        if (typeof no_accession==undefined){
+            meta_display_order.unshift('accession','strain');
+        } else{
+            meta_display_order.unshift('strain');
+        }
+
         create_dataTable('#'+meta_table_id, meta_display_order);
 
         //# assign data to each column
