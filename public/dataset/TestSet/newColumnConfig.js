49e2549ebccb39113f9092a9931ba15eabea39d2
wding <weiding8911@gmail.com>
2017-11-04 21:55:07 +0100

    add example of newColumnConfig.js

---
+++ public/dataset/TestSet/newColumnConfig.js
@@ -0,0 +1,11 @@
+var apply_association_cols=true;
+var new_columns_config=[
+    {insertion_pos:'Length', new_col: {'data':'', 'group name':'branch association', 'visible': true, 'tooltip':''}},
+    {insertion_pos:'Length', new_col: {'data':'association_test1 BA', 'group':'branch association', 'visible': true, 'name':'association_test1 BA','tooltip':'association_test1 branch association'}},
+    {insertion_pos:'Length', new_col: {'data':'association_test2 BA', 'group':'branch association', 'visible': true, 'name':'association_test2 BA','tooltip':'association_test2 branch association'}},
+
+    {insertion_pos:'Length', new_col: {'data':'', 'group name':'presence/absence association', 'visible': true, 'tooltip':''}},
+    {insertion_pos:'Length', new_col: {'data':'association_test1 PA', 'group':'presence/absence association', 'visible': true, 'name':'association_test1 PA','tooltip':'association_test1 presence/absence association'}},
+
+    {insertion_pos:'Length', new_col: {'data':'association_test2 PA', 'group':'presence/absence association', 'visible': true, 'name':'association_test2 PA','tooltip':'association_test2 presence/absence association'}}
+];
