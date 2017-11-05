4805313fc4e9a267da1719abe4a715cbd868746a
wding <weiding8911@gmail.com>
2017-11-05 10:25:03 +0100

    add

--- add-new-pages.py
+++ add-new-pages.py
@@ -4,7 +4,7 @@
 ## wide layout (gene cluster table for one row and alignment at the bottom):
 ## python add-new-pages.py -fn your_species -wl
 
-import os, sys, argparse
+import os, sys, json, argparse
 parser = argparse.ArgumentParser(description=\
     'Adding a new visualization instance for a custom pan-genome.',
     usage='python ./%(prog)s'+' -h (help)\n\
@@ -37,3 +37,28 @@ os.system('sed -i -- "s/TestSet/'+species+'/g" ./views/'+species+'.jade')
 ##process js file (in folder ./routes/)
 os.system('cp ./routes/TestSet.js ./routes/'+species+'.js')
 os.system('sed -i -- "s/TestSet/'+species+'/g" ./routes/'+species+'.js')
+
+##process association
+if params.show_associations:
+    with open('./public/dataset/TestSet/metaConfiguration.js'.replace('TestSet',species)) as meta_config:
+        for row in meta_config:
+            if 'association_columns=' in row:
+                cols=row.strip().split('association_columns=')[1].split(';')[0]
+                association_columns_list = [i for i in json.loads(cols)]
+    with open('./public/dataset/TestSet/newColumnConfig.js'.replace('TestSet',species),'wb') as newColumnConfig_output:
+        newColumnConfig_js='var apply_association_cols=true;\nvar new_columns_config=[\n'
+        ## add branch association group
+        newColumnConfig_js="%s  {insertion_pos:'Length', new_col: {'data':'', 'group name':'branch association', 'visible': true, 'tooltip':''}},\n"%(newColumnConfig_js)
+        ## add branch association columns
+        for i in association_columns_list:
+            newColumn_BA="    {insertion_pos:'Length', new_col: {'data':'newColumnName BA', 'group':'branch association', 'visible': true, 'name':'newColumnName BA','tooltip':'newColumnName branch association'}},\n".replace('newColumnName',i)
+            newColumnConfig_js="%s%s"%(newColumnConfig_js,newColumn_BA)
+
+        ## add presence/absence association group
+        newColumnConfig_js="%s  {insertion_pos:'Length', new_col: {'data':'', 'group name':'presence/absence association', 'visible': true, 'tooltip':''}},\n"%(newColumnConfig_js)
+        ## add presence/absence association columns
+        for i in association_columns_list:
+            newColumn_PA="    {insertion_pos:'Length', new_col: {'data':'newColumnName PA', 'group':'presence/absence association', 'visible': true, 'name':'newColumnName PA','tooltip':'newColumnName presence/absence association'}},\n".replace('newColumnName',i)
+            newColumnConfig_js="%s%s"%(newColumnConfig_js,newColumn_PA)
+        newColumnConfig_js+='];'
+        newColumnConfig_output.write(newColumnConfig_js)
