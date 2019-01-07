7240c185b753ef4e39a99e2acb4e577bd45f8561
aws_user <richard.neher@unibas.ch>
2019-01-07 15:00:25 +0000

    updated with current builds

---
+++ routes/HKFMRUKCJ_CampyCol_Basel.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('HKFMRUKCJ_CampyCol_Basel', { title: '' });
+  //res.sendfile(__dirname + '/public/clienthtml/index.html');
+});
+
+module.exports = router;
