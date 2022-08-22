f9caba15aab46e439b22d74f3b8f962d483f7378
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-08-23 01:56:09 +0200

    feat: allow setting data root url
    This is controlled by the env var `DATA_ROOT_URL` (see `.env.example`).
    
    `msa` library had to be patched to disable automatic CORS-proxying of absolute URLs through `https://cors-anywhere.herokuapp.com/`. I had to edit `/home/ia/work/aws_pangenome/public/javascripts/third_party/msa-niaid.js` and set defaults for the following configuration fields to an empty string:
    
    ```
    importProxy: ""
    importProxyStripHttp: ""
    ```

--- public/javascripts/data_path.js
+++ public/javascripts/data_path.js
@@ -1,17 +1,20 @@
+export const DATA_ROOT_URL = process.env.DATA_ROOT_URL
+export const DATASET_ROOT_URL=`${DATA_ROOT_URL}dataset/`
+
 /** data paths */
-export const path_datatable1="./dataset/"+speciesAbbr+"/geneCluster.json";
-export const coreTree_path="./dataset/"+speciesAbbr+"/coreGenomeTree.json";
-export const aln_file_path="./dataset/"+speciesAbbr+"/geneCluster/";
-export const path_datatable_meta="./dataset/"+speciesAbbr+"/strainMetainfo.json";
-//export const path_metaConfiguration="./dataset/"+speciesAbbr+"/metaDetails.json";
+export const path_datatable1=DATASET_ROOT_URL+speciesAbbr+"/geneCluster.json";
+export const coreTree_path=DATASET_ROOT_URL+speciesAbbr+"/coreGenomeTree.json";
+export const aln_file_path=DATASET_ROOT_URL+speciesAbbr+"/geneCluster/";
+export const path_datatable_meta=DATASET_ROOT_URL+speciesAbbr+"/strainMetainfo.json";
+//export const path_metaConfiguration=DATASET_ROOT_URL+speciesAbbr+"/metaDetails.json";
 
 if (typeof speciesAbbr2=='undefined') {
 	var speciesAbbr2=speciesAbbr+'2';
 }
-export const geneEvent_path_A='./dataset/'+speciesAbbr+'/geneGainLossEvent.json';
-export const geneEvent_path_B='./dataset/'+speciesAbbr2+'/geneGainLossEvent.json';
-export const path_datatable11="./dataset/"+speciesAbbr2+"/geneCluster.json";
-export const coreTree_path_B="./dataset/"+speciesAbbr2+"/coreGenomeTree.json";
-export const aln_file_path_B="./dataset/"+speciesAbbr2+"/geneCluster/";
-export const sequenceJsonPath="./dataset/"+speciesAbbr+"/_seq.json";
-//nodeAttriPath="./dataset/"+speciesAbbr+"/-tnt-nodeAttri.json"
+export const geneEvent_path_A=DATASET_ROOT_URL+speciesAbbr+'/geneGainLossEvent.json';
+export const geneEvent_path_B=DATASET_ROOT_URL+speciesAbbr2+'/geneGainLossEvent.json';
+export const path_datatable11=DATASET_ROOT_URL+speciesAbbr2+"/geneCluster.json";
+export const coreTree_path_B=DATASET_ROOT_URL+speciesAbbr2+"/coreGenomeTree.json";
+export const aln_file_path_B=DATASET_ROOT_URL+speciesAbbr2+"/geneCluster/";
+export const sequenceJsonPath=DATASET_ROOT_URL+speciesAbbr+"/_seq.json";
+//nodeAttriPath=DATASET_ROOT_URL+speciesAbbr+"/-tnt-nodeAttri.json"
--- scripts/renderPathogenHtml.js
+++ scripts/renderPathogenHtml.js
@@ -1,6 +1,14 @@
 const jade = require('jade')
 const fs = require('fs')
 const util = require('util')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const DATA_ROOT_URL = process.env.DATA_ROOT_URL
+if(!DATA_ROOT_URL) {
+  throw new Error('Environment variable \'DATA_ROOT_URL\' needs to be set')
+}
 
 const writeFile = util.promisify(fs.writeFile)
 
@@ -8,6 +16,7 @@ async function renderPathogenHtml(pathogen) {
   const html = jade.renderFile('views/pathogen_template.jade', {
     pretty: true,
     ...pathogen,
+    DATA_ROOT_URL,
   })
   return writeFile(`public/${pathogen.pathogenName}.html`, html)
 }
