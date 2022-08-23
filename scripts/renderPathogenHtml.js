8f39d7a2dc8484cff176bb3f75cfb876af6a07a3
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-08-23 06:28:29 +0200

    Revert "chore(ci): debug build script"
    This reverts commit 29f4dda35e671bf1bc486253e46b5105d2efabd5.

--- scripts/renderPathogenHtml.js
+++ scripts/renderPathogenHtml.js
@@ -34,7 +34,6 @@ async function getIndexJson(indexJsonPathOrUrl) {
 }
 
 function getIndexPathOrUrl() {
-  console.log({ argv: process.argv })
   const indexJsonPathOrUrl = process.argv[2]
   if(indexJsonPathOrUrl) {
     return indexJsonPathOrUrl
@@ -44,9 +43,7 @@ function getIndexPathOrUrl() {
 
 async function main() {
   const indexJsonPathOrUrl = getIndexPathOrUrl()
-  console.log({ indexJsonPathOrUrl })
   const indexJsonStr = await getIndexJson(indexJsonPathOrUrl)
-  console.log({ indexJsonStr })
   const indexJson = JSON.parse(indexJsonStr)
   return Promise.all(indexJson.datasets.map((pathogen) => renderPathogenHtml(pathogen)))
 }
